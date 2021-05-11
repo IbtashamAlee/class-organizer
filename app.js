53cabdf5fc0ba4026800a1eea83a7ae41aa07694
ibtashamalee <aleejaan562@gmail.com>
2021-05-12 00:39:13 +0500

    tests api

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "client-app/build")));
-    app.get("*", (req, res) => {
+   /* app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname + "/client-app/build/index.html"))
-    })
+    })*/
 } else {
     app.use(express.static(path.join(__dirname, 'public')));
 }
