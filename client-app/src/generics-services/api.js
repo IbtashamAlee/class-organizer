0a3f31e7c245d914af500fff411502afc9fd2509
ibtashamalee <aleejaan562@gmail.com>
2021-05-31 03:37:56 +0500

    adds generic api

--- client-app/src/generics-services/api.js
+++ client-app/src/generics-services/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 
 class Api {
     get = (url) => {
@@ -8,6 +8,31 @@ class Api {
                 resolve(res.data)
             }).catch(err => {
                 reject(err)
+            })
+        })
+    }
+}*/
+
+
+import axios from "axios";
+import qs from "qs";
+
+class Api {
+    constructor() {
+    }
+    execute(url,method, data={}) {
+        return new Promise(function (resolve, reject) {
+            axios({
+                method: method,
+                url: url,
+                data: qs.stringify(data),
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                }
+            }).then((res) =>{
+                resolve(res);
+            }).catch((err) =>{
+                reject(err);
             })
         })
     }
