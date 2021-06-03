1cea6d987103384acd6dc6b31e141bc933639c9d
ibtashamalee <aleejaan562@gmail.com>
2021-06-03 14:21:58 +0500

    uses protected route

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import {Switch, BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import React from 'react'
 import Home from "./components/home";
-import Footer from "./components/footer";
 import NotFound from "./components/not-found";
 import SignIn from "./components/signin";
 import SignUp from "./components/signup";
 import FAQ from "./components/faq";
 import Dashboard from "./components/dashboard";
+import {ProtectedRoute} from "./protected-route";
 
 function App() {
     return (
@@ -17,7 +17,8 @@ function App() {
                 <Route path="/signin" component={SignIn}/>
                 <Route path="/signup" component={SignUp}/>
                 <Route path="/faq" component={FAQ}/>
-                <Route path="/dashboard" component={Dashboard}/>
+
+                <ProtectedRoute exact path="/dashboard" component={Dashboard} />
                 <Route path="/" exact component={Home}/>
 
                 <Redirect to="/not-found"/>
