b643d6be18bfbee0ece655d81f0cba83a7ebb849
ibtashamalee <aleejaan562@gmail.com>
2021-06-07 05:40:49 +0500

    add delete class api

--- routes/classes.js
+++ routes/classes.js
@@ -4,11 +4,12 @@ let express = require('express');
 let router = express.Router();
 let Class = require('../models/ClassSchema');
 let jwt = require('jsonwebtoken');
+let getId = require('../validation-functions/get-id')
 
 router.post('/add', checkToken, async (req, res) => {
     let addClass = new Class({
         name: req.body.name,
-        userid: result.id
+        userid: getId(req.token)
     })
     addClass.save().then(() => {
         res.sendStatus(200);
@@ -17,6 +18,24 @@ router.post('/add', checkToken, async (req, res) => {
     });
 })
 
+router.post('/', checkToken, async (req,res) => {
+    let token = jwt.decode(req.token);
+    let allClasses;
+    await Class.find({userid: token.id}).then(async classes => {
+        allClasses = classes;
+        res.send(JSON.stringify(allClasses));
+    }).catch(err => {
+        res.sendStatus(409);
+    });
+})
 
+router.delete('/', checkToken, async (req, res) => {
+    await Class.findOne({_id: req.body.id}).then(delClass => {
+        delClass.remove();
+        res.sendStatus(200);
+    }).catch(err => {
+        res.sendStatus(409);
+    })
+})
 
 module.exports = router;
