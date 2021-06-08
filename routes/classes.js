24d4560082b7d5ca2a48ed4f295f6472bc0899a9
ibtashamalee <aleejaan562@gmail.com>
2021-06-09 03:58:47 +0500

    adds (add announcement) api

--- routes/classes.js
+++ routes/classes.js
@@ -4,7 +4,8 @@ let express = require('express');
 let router = express.Router();
 let Class = require('../models/ClassSchema');
 let jwt = require('jsonwebtoken');
-let getId = require('../validation-functions/get-id')
+let getId = require('../validation-functions/get-id');
+let Counter = require('../models/CounterSchema')
 
 router.post('/add', checkToken, async (req, res) => {
     let addClass = new Class({
@@ -27,6 +28,37 @@ router.post('/', checkToken, async (req,res) => {
     }).catch(err => {
         res.sendStatus(409);
     });
+})
+
+router.post('/announcement', checkToken, async (req, res) => {
+    let _class;
+    let counter;
+    Counter.findByIdAndUpdate({_id: req.body.class_id}, {$inc: { announcement_counter: 1} }, {new: true, upsert: true}).then(function(count) {
+    console.log("...count: "+JSON.stringify(count));
+    }).catch(err =>{
+        console.log(err);
+    })
+
+    Counter.findOne({_id: req.body.class_id}).then(result => {
+        console.log(result)
+        counter = result.announcement_counter;
+    }).catch(err => {
+        console.log(err);
+    });
+
+    await Class.findOne({_id: req.body.class_id }).then((result) => {
+        _class = result;
+        _class.annoucements.push({
+            id: counter,
+            announcement: req.body.annoucement
+        });
+
+        _class.save();
+        res.sendStatus(200);
+    }).catch(err => {
+        console.log(err);
+        res.sendStatus(409);
+    })
 })
 
 router.delete('/', checkToken, async (req, res) => {
