bc4d86ca64de38c50ec6b2b8dab9e6c6eb917a33
ibtashamalee <aleejaan562@gmail.com>
2021-06-11 02:54:34 +0500

    adds update announcement api and some other fixes

--- routes/classes.js
+++ routes/classes.js
@@ -5,10 +5,11 @@ let router = express.Router();
 let Class = require('../models/ClassSchema');
 let jwt = require('jsonwebtoken');
 let getId = require('../validation-functions/get-id');
-let Counter = require('../models/CounterSchema')
+let Counter = require('../models/CounterSchema');
+const isTutor = require('../middlewares/is-tutor')
 
 // add new class
-router.post('/add', checkToken, async (req, res) => {
+router.post('/add', checkToken, isTutor, async (req, res) => {
     let addClass = new Class({
         name: req.body.name,
         userid: getId(req.token)
@@ -21,17 +22,17 @@ router.post('/add', checkToken, async (req, res) => {
 })
 
 // delete class
-router.delete('/', checkToken, async (req, res) => {
-    await Class.findOne({_id: req.body.id}).then(delClass => {
+router.delete('/', checkToken, isTutor, async (req, res) => {
+    await Class.findOne({_id: req.body.class_id}).then(delClass => {
         delClass.remove();
         res.sendStatus(200);
     }).catch(err => {
-        res.sendStatus(409);
+        res.sendStatus(404);
     })
 })
 
 // get classes
-router.post('/', checkToken, async (req,res) => {
+router.post('/', checkToken, isTutor, async (req,res) => {
     let token = jwt.decode(req.token);
     let allClasses;
     await Class.find({userid: token.id}).then(async classes => {
@@ -43,39 +44,35 @@ router.post('/', checkToken, async (req,res) => {
 })
 
 // add new announcement
-router.post('/announcement', checkToken, async (req, res) => {
+router.post('/announcement', checkToken, isTutor, async (req, res) => {
     let _class;
-    let counter;
-    Counter.findByIdAndUpdate({_id: req.body.class_id}, {$inc: { announcement_counter: 1} }, {new: true, upsert: true}).then(function(count) {
-    }).catch(err =>{
-        console.log(err);
-    })
 
-    Counter.findOne({_id: req.body.class_id}).then(result => {
-        console.log(result)
-        counter = result.announcement_counter;
-    }).catch(err => {
-        console.log(err);
-    });
-
-    await Class.findOne({_id: req.body.class_id }).then((result) => {
+    await Class.findOne({_id: req.body.class_id }).then(async (result) => {
         _class = result;
-        _class.annoucements.push({
-            id: counter,
-            announcement: req.body.annoucement
-        });
+        await Counter.findByIdAndUpdate({_id: req.body.class_id}, {$inc: { announcement_counter: 1} }, {new: true, upsert: true}).then(function(count) {
+        }).catch(err =>{
+            console.log(err);
+        })
 
-        _class.save();
-        res.sendStatus(200);
+        await Counter.findOne({_id: req.body.class_id}).then(async result => {
+            _class.annoucements.push({
+                id: result.announcement_counter,
+                announcement: req.body.annoucement
+            });
+            _class.save();
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log(err);
+        });
     }).catch(err => {
-        console.log(err);
-        res.sendStatus(409);
+        res.status(404).send("Class not found!");
     })
+
 })
 
 // delete announcement
-router.delete('/announcement', checkToken, async (req, res) => {
-    Class.findOne({_id: req.body.class_id}).then(_class => {
+router.delete('/announcement',checkToken, isTutor, async (req, res) => {
+    await Class.findById(req.body.class_id).then(async _class => {
         let announcement = _class.annoucements.filter(item => item.id === parseInt(req.body.announcement_id));
         if (announcement.length === 0) {
             res.status(404).send("Unable to find announcement!");
@@ -83,6 +80,26 @@ router.delete('/announcement', checkToken, async (req, res) => {
         }
         let announcement_index = _class.annoucements.indexOf(announcement[0]);
         _class.annoucements.splice(announcement_index, 1);
+        _class.save();
+        res.sendStatus(200);
+    }).catch(() => {
+        res.sendStatus(404);
+    })
+})
+
+router.put('/announcement',checkToken, isTutor, async (req, res) => {
+    await Class.findById(req.body.class_id).then(async _class => {
+        let announcement = _class.annoucements.filter(item => item.id === parseInt(req.body.announcement_id));
+
+        if (announcement.length === 0) {
+            res.status(404).send("Unable to find announcement!");
+            return;
+        }
+
+        let announcement_index = _class.annoucements.indexOf(announcement[0]);
+        _class.annoucements.splice(announcement_index, 1);
+
+        _class.annoucements.push({id: parseInt(req.body.announcement_id), announcement: req.body.announcement});
         _class.save();
         res.sendStatus(200);
     }).catch(() => {
