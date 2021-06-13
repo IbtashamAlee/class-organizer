87ac210d21e5f5dea79c6b3fd598a3926b8f6d33
ibtashamalee <aleejaan562@gmail.com>
2021-06-13 05:26:55 +0500

    adds some dashboard functionality

--- client-app/src/components/dashboard.jsx
+++ client-app/src/components/dashboard.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Header from "./layout/header";
+import MediaCard from "./layout/card";
+import Api from '../generics-services/api'
 
-export default class Dashboard {
+export default class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    getClasses() {
+        Api.execute('/classes', 'get').then((res) => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    componentDidMount() {
+        this.getClasses();
+    }
+
     render() {
         return(
-            <div></div>
+            <div>
+                <Header/>
+                <MediaCard image="https://www.gstatic.com/classroom/themes/img_code.jpg"
+                           classname="Digital Image Processing" classdetails="FA18-BCS-B"
+                />
+            </div>
         )
     }
 }
