c6381ccf6e86d79f2c44af7c9f7df621f907af23
ibtashamalee <aleejaan562@gmail.com>
2021-06-16 01:05:39 +0500

    updates dashboard ui

--- client-app/src/components/dashboard.jsx
+++ client-app/src/components/dashboard.jsx
@@ -27,13 +27,16 @@ export default class Dashboard extends React.Component {
         return(
             <div>
                 <Header/>
-                <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 md:max-w-3xl lg:max-w-6xl mx-auto md:mt-28 xl:mt-10">
-                    {this.state.classes.length &&
+                <div className="text-gray-900 text-2xl font-medium mb-3 mt-24 xl:mt-10 md:max-w-3xl lg:max-w-6xl mx-auto max-w-xs">
+                    <p>All Classes</p>
+                </div>
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 md:max-w-3xl lg:max-w-6xl mx-auto max-w-xs md:mt-8 xl:mt-10">
+                    {this.state.classes.length ?
                     this.state.classes.map((item) => (
                         <MediaCard className="mx-auto" key={item._id} image={item.image} classname={item.name}
                                    classsection={item.section} classdetails={item.details}
                         />
-                    ))
+                    )) : <div></div>
                     }
                 </div>
             </div>
